45ef892f9aedd65c9c8acb2c5fffb21984a4921c
Shaaban <shabanhamdy94@gmail.com>
2023-09-17 19:03:58 +0300

    first commit

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
-import express from 'express'
+import  express  from 'express'
 import { initApp } from './src/utils/initApp.js'
 import { config } from 'dotenv'
-import ConnectionDB from './DB/Connection.js'
-import cors from "cors"
-const app = express()
-config({ path: "./Config/.env" })
-app.use(express.json({}))
-app.use(cors({}))
+config({path:"./Config/.env"})
 
-ConnectionDB()
-app.get('/', (req, res) => res.send('Hello World!'))
-
-
-
-
-initApp(app)
+initApp(express)
--- src/utils/initApp.js
+++ src/utils/initApp.js
@@ -1,22 +1,28 @@
+import ConnectionDB from "../../DB/Connection.js"
 import { globalErrorHandling } from "../middleWare/errorHandling.js"
 import * as allRoutes from "../modules/app.routes.js"
 
+import cors from "cors"
 
 
-export const initApp = (app) => {
-
+export const initApp =(express)=>{
+    const app = express()
     const port = process.env.PORT || 5001
-
+   app.use(cors({}))
+    
+    app.use(express.json({}))
+    ConnectionDB()
     // =============================================================================================
-    app.use('/user', allRoutes.userRouter)
-    app.use('/post', allRoutes.postRouter)
-
-    // =========================================
-    //====== error global =======================================
-    app.use(globalErrorHandling)
+    app.get('/', (req, res) => res.send('Hello World!'))
+    app.use('/user',allRoutes.userRouter )
+    app.use('/post',allRoutes.postRouter )
+    
+// =========================================
+//====== error global =======================================
+app.use(globalErrorHandling)
 
 
     // ==========================================================================================
     app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
+    
 }
